4a3e0000d9c623cd96ef691debbfb615c224d58c
Ramon29g <ramontrab674@gmail.com>
2026-07-29 17:46:01 +0000

    novo commit

--- # Personagem.java
+++ # Personagem.java
@@ -58,3 +58,113 @@ public abstract class Personagem {
         poderBase += valor;
     }
 }
+import java.util.ArrayList;
+
+// Classe Mago herda de Personagem e implementa Auditavel.
+public class Mago extends Personagem implements Auditavel {
+
+    private ArrayList<String> historicoAcoes;
+
+    public Mago(String nome, int nivel,
+                int pontosDeVida, double poderBase) {
+
+        super(nome, "Mago", nivel, pontosDeVida, poderBase);
+
+        historicoAcoes = new ArrayList<>();
+    }
+
+    @Override
+    public void usarHabilidade() {
+        System.out.println("Mago " + getNome()
+                + " lança uma poderosa Bola de Fogo!");
+    }
+
+    @Override
+    public void registrarAcao(String acao) {
+        historicoAcoes.add(acao);
+    }
+
+    @Override
+    public void auditarAcoes() {
+
+        System.out.println("Ações registradas por "
+                + getNome() + ":");
+
+        for (String acao : historicoAcoes) {
+            System.out.println("- " + acao);
+        }
+    }
+}
+// Classe Guerreiro herda de Personagem.
+public class Guerreiro extends Personagem {
+
+    public Guerreiro(String nome, int nivel,
+                     int pontosDeVida, double poderBase) {
+
+        super(nome, "Guerreiro", nivel,
+              pontosDeVida, poderBase);
+    }
+
+    @Override
+    public void usarHabilidade() {
+        System.out.println("Guerreiro " + getNome()
+                + " utiliza Golpe Devastador!");
+    }
+}
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Lista de personagens.
+        ArrayList<Personagem> personagens = new ArrayList<>();
+
+        // Adicionando magos.
+        personagens.add(
+                new Mago("Eldoran", 7, 120, 45.0));
+
+        personagens.add(
+                new Mago("Merlin", 10, 150, 60.0));
+
+        // Adicionando guerreiros.
+        personagens.add(
+                new Guerreiro("Arthus", 8, 220, 50.0));
+
+        personagens.add(
+                new Guerreiro("Ragnar", 12, 280, 70.0));
+
+        // Percorre a lista.
+        for (Personagem personagem : personagens) {
+
+            personagem.exibirStatus();
+
+            personagem.usarHabilidade();
+
+            // Verifica se é um Mago.
+            if (personagem instanceof Mago) {
+
+                Mago mago = (Mago) personagem;
+
+                mago.registrarAcao(
+                        "Conjurou uma magia especial.");
+
+                Auditavel auditavel = (Auditavel) mago;
+
+                auditavel.auditarAcoes();
+            }
+
+            // Casting explícito para Personagem.
+            Personagem referencia = (Personagem) personagem;
+
+            referencia.atribuirBencao(10.0);
+
+            System.out.println(
+                    "\nApós receber uma bênção:");
+
+            referencia.exibirStatus();
+
+            System.out.println();
+        }
+    }
+}
